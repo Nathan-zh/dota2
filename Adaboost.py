9a0516052a978755a762c070f45ff35c41c350a7
Qiang <zhangqiang@tamu.edu>
2018-12-10 16:09:29 -0600

    Final

--- Adaboost.py
+++ Adaboost.py
@@ -16,12 +16,12 @@ test = pd.read_csv(TEST_DATA_FILE, header=None)
 
 # y = array_train[:,0], x = array_train[:, 4:]
 # size: y: n*1, x: n*113
-array_train = train.values
+array_train = train.values[:3000, :]
 array_test = test.values
 
 # Single models
-names = ['Nearest Neighbors', 'SVM', 'Decision Tree', 'Neural Net', 'Naive Bayes']
-classifiers = [KNeighborsClassifier(3), SVC(), DecisionTreeClassifier(max_depth=5),
+names = ['Decision Tree', 'Nearest Neighbors', 'SVM', 'Neural Net', 'Naive Bayes']
+classifiers = [DecisionTreeClassifier(max_depth=5), KNeighborsClassifier(3), SVC(),
                MLPClassifier(alpha=0.55), GaussianNB()]
 clfs = [] # bad learners
 y_pres = [] # collection of train predictions
@@ -31,11 +31,11 @@ for name, classifier in zip(names, classifiers):
     classifier.fit(array_train[:, 4:], array_train[:, 0])
     clfs.append(classifier)
     y_pres.append(classifier.predict(array_train[:, 4:]))
+    acc_train = score(array_train[:, 0], y_pres[-1])
 
-    y_pre = classifier.predict(array_test[:, 4:])
     y_test_pres.append(classifier.predict(array_test[:, 4:]))
-    acc = score(array_test[:, 0], y_pre)
-    print('The accuracy of model %s is %.4f' % (name, acc))
+    acc = score(array_test[:, 0], y_test_pres[-1])
+    print('The accuracy of model %s is %.4f / %.4f' % (name, acc_train, acc))
 
 # Adaboost
 print('****************Adaboost start off!****************')
@@ -54,10 +54,11 @@ iteration = 1000
 w = []
 index = [min_index]
 for iters in range(iteration):
-    if (iters+1) % 100 == 0:
-        print('****************Iteration = ', iters+1, '****************')
+
     epsilon = np.dot(weights, np.abs(y_pres[index[-1]] - array_train[:, 0]) / 2)
-    w.append(0.5 * np.log(1 / epsilon - 1))
+    w.append(0.5 * np.log(1 / epsilon - 1) + 0.3)
+    if w[-1] < 0.01:
+        break
     temp = np.multiply(weights, np.exp(-w[-1] * np.multiply(y_pres[index[-1]], array_train[:, 0])))
     weights = temp / np.sum(temp)
     losses = []
@@ -68,14 +69,15 @@ for iters in range(iteration):
         losses.append(loss)
 
     index.append(np.argmin(losses))
+    print('Iteration = ', iters + 1, '  Index = ', index[-2], '  w = {:.4f}'.format(w[-1]))
 
 h_b = np.zeros(array_test[:, 0].shape)
 
-for k in range(iteration):
+for k in range(len(index)-1):
     h_b += w[k] * y_test_pres[index[k]]
 
 h_b = np.sign(h_b)
 acc = score(array_test[:, 0], h_b)
-print('The accuracy of ensemble model is %.4f' % acc)
+print('****************The accuracy of ensemble model is %.4f****************' % acc)
 
 
